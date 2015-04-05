14b115194764e3831029ddf3f07e3cf568c164be
rahil471 <rahilsk47@gmail.com>
2015-04-05 12:09:12 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ var notifyApp = angular.module('notifyApp',['angular-growl','ngAnimate']);
 
 notifyApp.controller('notifyCtrl',['$scope','growl',function($scope,growl){
 	$scope.showWarning = function(){
-		growl.warning('This is warning mesage.',{title: 'Warning!'});
+		growl.warning('This is a warning mesage.',{title: 'Warning!'});
 	}
 	$scope.showError = function(){
-		growl.error('This is error mesage.',{title: 'Error!'});
+		growl.error('This is a error mesage.',{title: 'Error!'});
 	}
 	$scope.showSuccess = function(){
-		growl.success('This is success mesage.',{title: 'Success!'});
+		growl.success('This is a success mesage.',{title: 'Success!'});
 	}
 	$scope.showInfo = function(){
 		growl.info('This is an info mesage.',{title: 'Info!'});
